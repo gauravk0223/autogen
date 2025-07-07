6e25ef16dfc953eda1ac19a0ab444282a631d275
“Gaurav <gak0223@gmail.com>
2025-07-07 10:07:20 +0530

    Added code for Tool in Autogen Agents

---
+++ 2. Autogen Basics/autogen_custom_tool.py
@@ -0,0 +1,7 @@
+from autogen_core.tools import FunctionTool
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
